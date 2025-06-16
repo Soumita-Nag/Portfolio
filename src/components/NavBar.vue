ec3dc053962f0c4932e598a21b1e1790d96c17cb
Soumita Nag <168756525+Soumita-Nag@users.noreply.github.com>
2025-06-16 22:43:13 +0530

    bug fixed: navber position sticky

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,5 +1,5 @@
 <template>
-  <section class="flex justify-between items-center p-6 relative z-20 bg-opacity-50">
+  <section class="sticky top-0 z-50 flex justify-between items-center p-6 bg-[var(--color-bg)] bg-opacity-90 backdrop-blur-md">
     <div class="text-white text-3xl font-bold">
       Soumita <span class="text-[var(--color-primary)]">Nag</span>
     </div>
