d9ff648a76a429494eb1af89e4463b783a964a9f
Soumita Nag <168756525+Soumita-Nag@users.noreply.github.com>
2025-06-21 02:30:13 +0530

    skill segment grid modified

--- src/components/skills.vue
+++ src/components/skills.vue
@@ -6,15 +6,15 @@
         <span class="text-transparent bg-clip-text bg-gradient-to-r from-[var(--color-primary)] to-[var(--color-secondary)]">Skills</span>
       </h2>
 
-      <div class="grid md:grid-cols-2 gap-10">
+      <div class="grid md:grid-cols-2 lg:grid-cols-3 gap-10">
         <div v-for="(group, index) in skillCategories" :key="index" class="space-y-6">
           <h3 class="text-2xl font-semibold" data-aos="fade-up">{{ group.title }}</h3>
 
           <!-- Skills -->
           <div v-for="skill in group.skills" :key="skill.id" class="mb-4" data-aos="fade-up">
             <div class="flex items-center justify-between">
-              <span class="font-semibold md:w-[10%] w-[15%] uppercase">{{ skill.name }}</span>
-              <div class="mt-2 h-1 lg:w-[80%] w-[50%] bg-[#1f2f4b] rounded-full">
+              <span class="font-semibold md:w-[15%] w-[15%] uppercase">{{ skill.name }}</span>
+              <div class="mt-2 h-1 mr-[10%] lg:w-[60%] w-[50%] bg-[#1f2f4b] rounded-full">
                 <div class="h-1 rounded-full bg-gradient-to-r from-[var(--color-primary)] to-[var(--color-secondary)]" :style="`width: ${skill.width}`"></div>
               </div>
               <!-- <span class="font-semibold w-[8%] lg:w-[10%] text-transparent bg-clip-text bg-gradient-to-r from-[var(--color-primary)] to-[var(--color-secondary)]">{{ skill.width }}</span> -->
@@ -32,49 +32,40 @@ export default {
     return {
       skillCategories: [
         {
-          title: "1. Programming Languages",
+          title: "1. Programming & Scripting Languages",
           skills: [
             { id: 1, name: 'C', width: '85%' },
             { id: 2, name: 'C++', width: '90%' },
             { id: 3, name: 'Java', width: '80%' },
             { id: 4, name: 'Python', width: '70%' },
             { id: 5, name: 'JavaScript', width: '85%' },
+            { id: 6, name: 'php', width: '80%' },
+            { id: 7, name: 'sql', width: '85%' },
           ]
         },
         {
-          title: "2. Web Development (Frontend)",
+          title: "2. Web & Database Technologies",
           skills: [
-            { id: 7, name: 'HTML', width: '95%' },
-            { id: 8, name: 'CSS', width: '90%' },
-            { id: 9, name: 'Tailwind CSS', width: '90%' },
-            { id: 11, name: 'Vue.js', width: '90%' }
-          ]
-        },
-        {
-          title: "3. Web Development (Backend)",
-          skills: [
+            { id: 8, name: 'HTML', width: '95%' },
+            { id: 9, name: 'CSS', width: '90%' },
+            { id: 10, name: 'Tailwind CSS', width: '90%' },
+            { id: 11, name: 'Vue.js', width: '90%' },
             { id: 12, name: 'Node.js', width: '90%' },
             { id: 13, name: 'Express.js', width: '90%' },
-            { id: 14, name: 'PHP', width: '75%' }
+            { id: 14, name: 'MongoDB', width: '80%' },
+            { id: 15, name: 'MySQL', width: '85%' },
           ]
         },
         {
-          title: "4. Databases",
+          title: "3. Tools & Development Platforms",
           skills: [
-            { id: 15, name: 'MySQL', width: '85%' },
-            { id: 16, name: 'MongoDB', width: '80%' }
+            { id: 16, name: 'Git', width: '85%' },
+            { id: 17, name: 'Postman', width: '75%' },
+            { id: 18, name: 'GitHub', width: '85%' },
+            { id: 19, name: 'VS Code', width: '90%' },
+            { id: 20, name: 'Jupyter Notebook', width: '70%' }
           ]
         },
-        // {
-        //   title: "5. Developer Tools & Platforms",
-        //   skills: [
-        //     { id: 17, name: 'Git', width: '85%' },
-        //     { id: 18, name: 'GitHub', width: '85%' },
-        //     { id: 19, name: 'Postman', width: '75%' },
-        //     { id: 20, name: 'VS Code', width: '90%' },
-        //     { id: 21, name: 'Jupyter Notebook', width: '70%' }
-        //   ]
-        // }
       ]
     };
   }
@@ -82,5 +73,4 @@ export default {
 </script>
 
 <style scoped>
-/* Tailwind handles styling */
 </style>
